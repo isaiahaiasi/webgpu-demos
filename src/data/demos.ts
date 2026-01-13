a44891370ee6e782161eaf24ad7b28060e713739
Isaiah A <isaiah.althouse@gmail.com>
2026-01-12 21:36:07 -0700

    Refactor demo data file

---
+++ src/data/demos.ts
@@ -0,0 +1,15 @@
+export const demoData = {
+    "triangle": {
+        title: "Triangle",
+        order: 0,
+        description: "\"Hello World\" of WebGPU",
+    },
+    "slime-mold": {
+        title: "Slime Mold",
+        
+    },
+    "game-of-life": {
+        title: "Game of Life",
+        description: "Basic intro to cellular automata.",
+    },
+};
